584aedc50a6d0d6597736eee96ca7913c9fc8f4f
Gilles Henrard <gilles.henrard@hotmail.com>
2018-02-09 22:49:21 +0100

    attempt implementing badge view

--- web/urls.py
+++ web/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name="home"),
-#    url(r'badge/(?P<FN_search>[A-z -]*)$', views.badge, name="badge"),
+    url(r'badge/(?P<FN_search>[A-z -]*)$', views.badge, name="badge"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from web.forms import PersonForm
+from api.serializers import PersonSerializer
 from django.template.context_processors import request
 from django.core.urlresolvers import reverse
 from rest_framework.status import HTTP_404_NOT_FOUND
+from api.models import Person
 import requests
 
 def home(request):
@@ -10,14 +12,23 @@ def home(request):
     r = requests.get("http://localhost:8000" + reverse("get_post_people"))
     return render(request, 'web/home.html', {'people' : r.json()})
 
-# def badge(request, FN_search):
-#     person = get_object_or_404(Person, firstname=FN_search)
-#     
-#     form = PersonForm(request.POST or None, request.FILES, instance=person)
+def badge(request, FN_search):
+    r = requests.get("http://localhost:8000" + reverse("get_post_people"), data={'fstname' : FN_search})
+    serializer = PersonSerializer(data=r)
+    if serializer.is_valid():
+        print(serializer)
+        form = PersonForm(request.POST or None, request.FILES, instance=serializer.data)
+        print(form)
+    
+    return HTTP_404_NOT_FOUND
+
+#    person = get_object_or_404(Person, firstname=FN_search)
+     
+#     form = PersonForm(request.POST or None, request.FILES, instance=r.json())
 #     confirm = False
 #     if form.is_valid():
 #         FN_search=request.POST["firstname"]
 #         form.save()
 #         confirm = True
-#         
+#          
 #     return render(request, 'web/badge.html', {'form':form, 'FN_search':FN_search, 'confirm':confirm,})
