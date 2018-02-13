bcae3568f449469039d2b77366cda549e036125e
Gilles Henrard <gilles.henrard@hotmail.com>
2018-02-13 11:53:21 +0100

    remove useless call

--- web/views.py
+++ web/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from web.forms import PersonForm
 from django.core.urlresolvers import reverse
 from requests import get, put
-from rest_framework import status
 
 def home(request):
         r = get("http://localhost:8000" + reverse("get_post_people"))
