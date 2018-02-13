a20756d2637994ec677e8aa6b5be94345e3961f8
Gilles Henrard <gilles.henrard@hotmail.com>
2018-02-13 16:45:20 +0100

    add a few unit tests to web.tests.test_views

--- web/tests/test_views.py
+++ web/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.core.urlresolvers import reverse, NoReverseMatch
 from rest_framework import status
+from api.models import Person
 
 # initialize the APIClient app
 client = Client()
@@ -18,3 +19,21 @@ class homeTest(TestCase):
         #get home page with parameters
         with self.assertRaises(NoReverseMatch):
             client.get(reverse('home', args=['test']))
+
+class badgeTest(TestCase):
+    """Test module for values on Badge page"""
+
+    def setUp(self):
+        self.jane = Person.objects.create(
+            firstname='Jane', lastname='Dean', country='US', email='test2@test.com', phone='+1123456789', occupation_field='Administration', occupation='Accountance', birthdate='1982-05-01', description='Smart girl')
+        self.jane = Person.objects.create(
+            firstname='Jane', lastname='Dean', country='US', email='test2@test.com', phone='+1123456789', occupation_field='Administration', occupation='Accountance', birthdate='1982-05-01', description='Smart girl')
+
+    def test_valid_get_badge(self):
+        response = client.get(reverse('badge', kwargs={'FN_search' : self.jane.firstname}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTemplateUsed(response, 'web/badge.html')
+    
+    def test_invalid_method_badge(self):
+        response = client.put(reverse('badge', kwargs={'FN_search' : self.jane.firstname}))
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
