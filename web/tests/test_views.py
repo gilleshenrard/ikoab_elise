40c8e11ff3c0d636b0638f665b86a1064b4973c2
Gilles Henrard <gilles.henrard@hotmail.com>
2018-02-14 17:18:32 +0100

    add a few unit tests on web.views

--- web/tests/test_views.py
+++ web/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase, Client
 from django.core.urlresolvers import reverse, NoReverseMatch
 from rest_framework import status
 from api.models import Person
+import json
 
 # initialize the APIClient app
 client = Client()
@@ -24,16 +25,35 @@ class badgeTest(TestCase):
     """Test module for values on Badge page"""
 
     def setUp(self):
-        self.jane = Person.objects.create(
-            firstname='Jane', lastname='Dean', country='US', email='test2@test.com', phone='+1123456789', occupation_field='Administration', occupation='Accountance', birthdate='1982-05-01', description='Smart girl')
-        self.jane = Person.objects.create(
-            firstname='Jane', lastname='Dean', country='US', email='test2@test.com', phone='+1123456789', occupation_field='Administration', occupation='Accountance', birthdate='1982-05-01', description='Smart girl')
+        self.john = Person.objects.create(
+            firstname='John', lastname='Doe', country='UK', email='test@test.com', phone='+44123456789', occupation_field='Diplomacy', occupation='Spy', birthdate='1963-05-01', description='Tall guy')
+        self.john2 = {
+            'firstname' : 'John',
+            'lastname' : 'Doe2',
+            'country' : 'UK2',
+            'email' : 'test2@test.com',
+            'phone' : '+441234567892',
+            'occupation_field' : 'Diplomacy2',
+            'occupation' : 'Spy2',
+            'birthdate' : '1963-05-02',
+            'description' : 'Tall guy2'}
 
     def test_valid_get_badge(self):
-        response = client.get(reverse('badge', kwargs={'FN_search' : self.jane.firstname}))
+        response = client.get(reverse('badge', kwargs={'FN_search' : self.john.firstname}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTemplateUsed(response, 'web/badge.html')
+
+    def test_invalid_firstname_get_badge(self):
+        response = client.get(reverse('badge', kwargs={'FN_search' : 'test'}))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTemplateUsed(response, 'web/badge.html')
+
+    def test_valid_post_badge(self):
+        response = client.post(reverse('badge', kwargs={'FN_search' : self.john.firstname}), data=json.dumps(self.john2), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTemplateUsed(response, 'web/badge.html')
     
-    def test_invalid_method_badge(self):
-        response = client.put(reverse('badge', kwargs={'FN_search' : self.jane.firstname}))
-        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    def test_invalid_firstname_post_badge(self):
+        response = client.post(reverse('badge', kwargs={'FN_search' : 'test'}), data=json.dumps(self.john2), content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertTemplateUsed(response, 'web/badge.html')
